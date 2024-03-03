99df6e6d9c633923970f5383a371aab408b3c190
RAMBABU <chinnak7416@gmail.com>
2024-03-03 14:24:38 +0530

    moving the heading,printing to a function ,Also adding same massage to output as well

--- backend.sh
+++ backend.sh
@@ -1,64 +1,69 @@
 mysql_root_password=$1
-echo disable default nodejs version
+
+print_task_heading() {
+  echo $1
+  echo "########## $1 ###########" &>>/tmp/expense.log
+}
+print_task_heading "disable default nodejs version"
 dnf module disable nodejs -y &>>/tmp/expense.log
 echo $?
 
-echo enable nodejs version:20
+print_task_heading "enable nodejs version:20"
 dnf module enable nodejs:20 -y &>>/tmp/expense.log
 echo $?
 
-echo install nodejs
+print_task_heading "install nodejs"
 dnf install nodejs -y &>>/tmp/expense.log
 echo $?
 
-echo adding apliction user
+print_task_heading "adding apliction user"
 useradd expense &>>/tmp/expense.log
 echo $?
 
-echo copy backend service file
+print_task_heading "copy backend service file"
 cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
 echo $?
 
-echo clean the old content
+print_task_heading "clean the old content"
 rm -rf /app &>>/tmp/expense.log
 echo $?
 
-echo create app directory
+print_task_heading "create app directory"
 mkdir /app &>>/tmp/expense.log
 echo $?
 
-echo download app content
+print_task_heading "download app content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
 echo $?
 
-echo extract app content
+print_task_heading "extract app content"
 cd /app &>>/tmp/expense.log
 unzip /tmp/backend.zip &>>/tmp/expense.log
 echo $?
 
-echo download nodjs dependencies
+print_task_heading "download nodjs dependencies"
 cd /app &>>/tmp/expense.log
 npm install &>>/tmp/expense.log
 echo $?
 
-echo reload the service
+print_task_heading "reload the service"
 systemctl daemon-reload &>>/tmp/expense.log
 echo $?
 
-echo start backend service
+print_task_heading "start backend service"
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend &>>/tmp/expense.log
 echo $?
 
-echo install mysql client
+print_task_heading "install mysql client"
 dnf install mysql -y &>>/tmp/expense.log
 echo $?
 
-echo load schema
+print_task_heading "load schema"
 mysql -h 172.31.5.244 -uroot -p${mysql_root_password} < /app/schema/backend.sql  &>>/tmp/expense.log
 echo $?
 
-echo restart bckend
+print_task_heading ""restart bckend""
 systemctl restart backend &>>/tmp/expense.log
 echo $?
 
