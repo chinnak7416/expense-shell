8432335eb9cd72f3a955b85501eeebefa54612dd
RAMBABU <chinnak7416@gmail.com>
2024-03-05 20:34:08 +0530

    Adding input pass to mysql

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,10 @@
 source common.sh
-
+mysql_root_password=$1
+#If password is not provide then exit
+if [ -z "${mysql_root_password}" ]; then
+  echo Input password is missing.
+  exit 1
+fi
 print_task_heading "Install mysql-server"
 dnf install mysql-server -y &>>$LOG
 check_status $?
@@ -13,5 +18,5 @@ systemctl start mysqld &>>$LOG
 check_status $?
 
 print_task_heading "Set root password"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOG
 check_status $?
