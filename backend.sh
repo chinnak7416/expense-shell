e667cbd63b1c7482b7930ee7ee48e56baeadc2de
RAMBABU <chinnak7416@gmail.com>
2024-03-05 18:44:12 +0530

    Handling if password is missing in backend

--- backend.sh
+++ backend.sh
@@ -1,7 +1,7 @@
 source common.sh
 mysql_root_password=$1
 #If password is not provide then exit
-if [ -z "${mysql_root_password}"]; then
+if [ -z "${mysql_root_password}" ]; then
   echo Input password is missing.
   exit 1
 fi
